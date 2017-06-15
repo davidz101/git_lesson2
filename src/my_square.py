e75a126bd150ae3d45c44e61cd629b5e3af84f9a
David Zhuang <dizhuang@cisco.com>
2017-06-14 17:38:39 -0700

    change x to y, 4 to 42

--- src/my_square.py
+++ src/my_square.py
@@ -1,8 +1,8 @@
-def my_square(x):
+def my_square(y):
     """ takes a value adn return
     
     uses the ** 
     """
-    return (x **2)
+    return (y **2)
 
-print(my_square(4))
+print(my_square(42))
